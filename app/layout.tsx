a1c3fbee8193601b274912746e4382f16948745f
VishalR3 <vishal.ch.36@gmail.com>
2025-03-20 01:41:21 +0530

    fix for ios

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,8 @@ export default function RootLayout({
               width: "100dvw",
               position: "fixed",
               bottom: 0,
-              height: "4rem",
+              height: "4rem + env(safe-area-inset-bottom)",
+              paddingBottom: "env(safe-area-inset-bottom)",
             }}
           >
             <BottomNavigation />
